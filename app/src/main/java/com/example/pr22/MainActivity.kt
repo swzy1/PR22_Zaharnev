c103adff70446d6da846c89f537cc7a41cd0d37a
swzy1 <skidulya@gmail.com>
2024-12-20 14:37:53 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/pr22/MainActivity.kt
+++ app/src/main/java/com/example/pr22/MainActivity.kt
@@ -7,14 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import com.example.memorygame.MemoryGameApp
-import com.example.pr22.ui.theme.PR22_NikolaenkoTheme
+import com.example.pr22.ui.theme.PR22_ZakharnevTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PR22_NikolaenkoTheme {
+            PR22_ZakharnevTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
                     MemoryGameApp()
                 }
